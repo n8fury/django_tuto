7a6a74e970ae15fc05bed351fef573bbe8a1c14b
n8fury <mhjoy547@yahoo.com>
2024-09-10 15:10:16 +0600

    added  templating && git push

--- test_project/urls.py
+++ test_project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello_world), # home page
+    path('', home),  # home page
+    path('/hello', hello_world),  # hello page
 ]
