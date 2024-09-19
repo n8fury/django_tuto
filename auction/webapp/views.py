31ee4627e6f62aaaf656ea3edb5fc61879dde4dd
n8fury <mhjoy547@yahoo.com>
2024-09-19 14:12:56 +0600

    added login

--- auction/webapp/views.py
+++ auction/webapp/views.py
@@ -45,3 +45,18 @@ def register(request):
 
 
 # Login
+def login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authnticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                # return redirect('')
+
+    context = {'form': form}
+    return render(request, 'webapp/login.html', context=context)
